64656f566707fa8d129a57454355ad58d6c8e65e
Golden1636 <okeamagolden@yahoo.com>
2023-03-21 08:11:22 +0100

    vans and vandetail pages implemented with react-router-dom

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import "./app.css";
 import Home from "./pages/Home";
 import About from "./pages/About";
+import Vans from "./pages/Vans";
+import VanDetail from "./pages/VanDetail";
+
+import "./server";
 
 const App = () => {
   return (
@@ -12,11 +16,14 @@ const App = () => {
         </Link>
         <nav>
           <Link to="/about">About</Link>
+          <Link to="/vans">Vans</Link>
         </nav>
       </header>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
+        <Route path="/vans" element={<Vans />} />
+        <Route path="/vans/:id" element={<VanDetail />} />
       </Routes>
     </BrowserRouter>
   );
